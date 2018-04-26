8c416371f80da1418c5a904c11893d8b27c21801
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-04-26 23:01:14 +1000

    Change to ScrollView

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import { Header, Card, Button } from 'react-native-elements'
 import { Font } from 'expo'
 
@@ -23,7 +23,7 @@ export default class App extends React.Component {
   render() {
     return !!this.state.fontLoaded ? (
       // <View style={styles.container}>
-      <View>
+      <ScrollView>
         <Header
           leftComponent={{ icon: 'menu', color: '#fff' }}
           centerComponent={{ text: 'Glenn', style: { color: '#fff' } }}
@@ -44,9 +44,37 @@ export default class App extends React.Component {
             onPress={() => { console.log('View Now Pressed') }} />
         </Card>
 
-        
+        <Card
+          title='HELLO WORLD'
+          image={require('./assets/images/frenchie-small.jpg')}>
+          <Text style={{marginBottom: 10}}>
+            The idea with React Native Elements is more about component structure than actual design.
+          </Text>
+          <Button
+            icon={{name: 'code'}}
+            backgroundColor='#03A9F4'
+            fontFamily='LatoRegular'
+            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+            title='VIEW NOW'
+            onPress={() => { console.log('View Now Pressed') }} />
+        </Card>
 
-      </View>
+        <Card
+          title='HELLO WORLD'
+          image={require('./assets/images/frenchie-small.jpg')}>
+          <Text style={{marginBottom: 10}}>
+            The idea with React Native Elements is more about component structure than actual design.
+          </Text>
+          <Button
+            icon={{name: 'code'}}
+            backgroundColor='#03A9F4'
+            fontFamily='LatoRegular'
+            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+            title='VIEW NOW'
+            onPress={() => { console.log('View Now Pressed') }} />
+        </Card>
+
+      </ScrollView>
     ) : <View><Text>Loading...</Text></View>
   }
 }
