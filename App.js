8fb35ba72ce57d57e4580b0066797c951c147550
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-04-27 19:50:53 +1000

    Update layout

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import { Header, Card, Button } from 'react-native-elements'
+import { Header, Card, Button, ButtonGroup } from 'react-native-elements'
 import { Font } from 'expo'
 
 export default class App extends React.Component {
   state = {
-    fontLoaded: false
+    fontLoaded: false,
+    selectedIndex: null
   }
 
   async componentDidMount() {
@@ -20,70 +21,97 @@ export default class App extends React.Component {
     }
   }
 
+  updateIndex (selectedIndex) {
+    this.setState({
+      selectedIndex: selectedIndex
+    })
+  }
+
   render() {
+    const buttons = ['Menu 1', 'Menu 2', 'Menu 3']
+    const { selectedIndex } = this.state
+
     return !!this.state.fontLoaded ? (
-      // <View style={styles.container}>
-      <ScrollView>
-        <Header
-          leftComponent={{ icon: 'menu', color: '#fff' }}
-          centerComponent={{ text: 'Glenn', style: { color: '#fff' } }}
-          rightComponent={{ icon: 'home', color: '#fff' }} />
+      <View style={styles.container}>
+        
+        <View>
+          <Header
+            leftComponent={{ icon: 'menu', color: '#fff' }}
+            centerComponent={{ text: 'Glenn', style: { color: '#fff' } }}
+            rightComponent={{ icon: 'home', color: '#fff' }} />
 
-        <Card
-          title='HELLO WORLD'
-          image={require('./assets/images/frenchie-small.jpg')}>
-          <Text style={{marginBottom: 10}}>
-            The idea with React Native Elements is more about component structure than actual design.
-          </Text>
-          <Button
-            icon={{name: 'code'}}
-            backgroundColor='#03A9F4'
-            fontFamily='LatoRegular'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW'
-            onPress={() => { console.log('View Now Pressed') }} />
-        </Card>
+          <ButtonGroup
+                onPress={this.updateIndex.bind(this)}
+                selectedIndex={selectedIndex}
+                buttons={buttons}
+                containerStyle={{height: 40}}
+              />
+        </View>
 
-        <Card
-          title='HELLO WORLD'
-          image={require('./assets/images/frenchie-small.jpg')}>
-          <Text style={{marginBottom: 10}}>
-            The idea with React Native Elements is more about component structure than actual design.
-          </Text>
-          <Button
-            icon={{name: 'code'}}
-            backgroundColor='#03A9F4'
-            fontFamily='LatoRegular'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW'
-            onPress={() => { console.log('View Now Pressed') }} />
-        </Card>
+        <ScrollView style={{flex: 1 /* Set Scrollview to 100% height */}}>
+          <Card
+            title='HELLO WORLD'
+            image={require('./assets/images/frenchie-small.jpg')}>
+            <Text style={{marginBottom: 10}}>
+              The idea with React Native Elements is more about component structure than actual design.
+            </Text>
+            <Button
+              icon={{name: 'code'}}
+              backgroundColor='#03A9F4'
+              fontFamily='LatoRegular'
+              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+              title='VIEW NOW'
+              onPress={() => { console.log('View Now Pressed') }} />
+          </Card>
 
-        <Card
-          title='HELLO WORLD'
-          image={require('./assets/images/frenchie-small.jpg')}>
-          <Text style={{marginBottom: 10}}>
-            The idea with React Native Elements is more about component structure than actual design.
-          </Text>
-          <Button
-            icon={{name: 'code'}}
-            backgroundColor='#03A9F4'
-            fontFamily='LatoRegular'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW'
-            onPress={() => { console.log('View Now Pressed') }} />
-        </Card>
+          <Card
+            title='HELLO WORLD'
+            image={require('./assets/images/frenchie-small.jpg')}>
+            <Text style={{marginBottom: 10}}>
+              The idea with React Native Elements is more about component structure than actual design.
+            </Text>
+            <Button
+              icon={{name: 'code'}}
+              backgroundColor='#03A9F4'
+              fontFamily='LatoRegular'
+              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+              title='VIEW NOW'
+              onPress={() => { console.log('View Now Pressed') }} />
+          </Card>
 
-      </ScrollView>
+          <Card
+            title='HELLO WORLD'
+            image={require('./assets/images/frenchie-small.jpg')}>
+            <Text style={{marginBottom: 10}}>
+              The idea with React Native Elements is more about component structure than actual design.
+            </Text>
+            <Button
+              icon={{name: 'code'}}
+              backgroundColor='#03A9F4'
+              fontFamily='LatoRegular'
+              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+              title='VIEW NOW'
+              onPress={() => { console.log('View Now Pressed') }} />
+          </Card>
+        </ScrollView>
+        <View style={{height: 50}}>
+          <ButtonGroup
+            onPress={this.updateIndex.bind(this)}
+            selectedIndex={selectedIndex}
+            buttons={buttons}
+            containerStyle={{height: 50}}
+          />
+        </View>
+      </View>
     ) : <View><Text>Loading...</Text></View>
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center'
-//   }
-// })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'//,
+    // alignItems: 'center',
+    // justifyContent: 'center'
+  }
+})
